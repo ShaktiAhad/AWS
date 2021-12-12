dad0563b7a6f994f027acd4d5916e08122be9f15
Ahad Noor Shakti <53283457+ShaktiAhad@users.noreply.github.com>
2021-12-13 01:27:11 +0900

    Update yaml_test.py

--- untitled folder/yaml_test.py
+++ untitled folder/yaml_test.py
@@ -11,12 +11,35 @@ oc login --token={TOKEN} --server={SERVER_URL}
 oc project {SOURCE_PROJECT}
 ''')
 
+def dc(dc_content):
+    if dc_content['spec']['replicas'] >= 1: dc_content['spec']['replicas']=0
+
+    metadata_image = (dc_content['spec']['template']['spec']['containers'][0]['image'].split('/'))
+    metadata_image[1] = f"{DESTINATION_PROJECT}"
+    metadata_image_path='/'.join(metadata_image)
+
+    annotation_value_update = json.loads(dc_content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration'])
+
+    annotation_image = annotation_value_update['spec']['template']['spec']['containers'][0]['image'].split('/') 
+    annotation_image[1]=f"{DESTINATION_PROJECT}"
+    annotation_image_path='/'.join(annotation_image)
+
+    # annotation_value_update['metadata']['namespace']=f"{DESTINATION_PROJECT}" # update annotation namespace
+    annotation_value_update['spec']['template']['spec']['containers'][0]['image']=annotation_image_path # update annotation image
+
+
+    # dc_content['metadata']['namespace'] = f"{DESTINATION_PROJECT}" # for main namespace
+    dc_content['spec']['template']['spec']['containers'][0]['image'] = metadata_image_path # for main image
+    dc_content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration']= json.dumps(annotation_value_update) # for annotation namespace and image path as string
+
 def common(value):
     file_configs = os.popen(f'oc get {value} -o name').read().split('\n')
     file_configs.remove('')
     configs =[]
     for config in file_configs:
-        configs.append(config.split('/').pop(1))
+        if {value} == "route" and config.endswith("intraxa"): configs.append(config.split('/').pop(1))
+        elif {value} == "svc" and not config.startswith("egress"): configs.append(config.split('/').pop(1))
+        else: configs.append(config.split('/').pop(1))
 
     for each_config in configs:
         os.system(f'oc get {value} {each_config} -o yaml > file.yaml')
@@ -28,37 +51,43 @@ def common(value):
             content['metadata'].pop(item, None)
         content.pop('status', None)
 
-        if value == "dc":
-            if content['spec']['replicas'] >= 1: content['spec']['replicas']=0
-            # metadata_image_path = (content['spec']['template']['spec']['containers'][0]['image'].split('/'))
-            # metadata_image_path[1] = f"{DESTINATION_PROJECT}"
-            # '/'.join(metadata_image_path)
-
-            # annotation_value_update = json.loads(content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration'])
-
-            # annotation_image_path = annotation_value_update['spec']['template']['spec']['containers'][0]['image'].split('/') 
-            # annotation_image_path[1]=f"{DESTINATION_PROJECT}"
-            # '/'.join(annotation_image_path)
-
-            # annotation_value_update['metadata']['namespace']=f"{DESTINATION_PROJECT}" # update annotation namespace
-            # annotation_value_update['spec']['template']['spec']['containers'][0]['image']=annotation_image_path # update annotation image
-
-
-            # content['metadata']['namespace'] = f"{DESTINATION_PROJECT}" # for namespace
-            # content['spec']['template']['spec']['containers'][0]['image'] = metadata_image_path # for image
-            # content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration']= json.dumps(annotation_value_update)
-        
-        if value == "svc":
+        if value == "dc" and content['spec']['replicas'] >= 1: content['spec']['replicas']=0
+        elif value == "is": content['spec'].pop('tags', None)
+        elif value == "svc":
             content['metadata'].pop('selfLink', None)
             content['spec'].pop('clusterIP', None)
             content['spec'].pop('clusterIPs', None)
 
+
+        # rename common namespace for all configs
+        if value == "dc" or "is" or "svc" or "route":
+            annotation_value_update = json.loads(content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration'])
+            annotation_value_update['metadata']['namespace']=f"{DESTINATION_PROJECT}" # update annotation namespace
+            content['metadata']['namespace'] = f"{DESTINATION_PROJECT}" # for main namespace
+            content['metadata']['annotations']['kubectl.kubernetes.io/last-applied-configuration']= json.dumps(annotation_value_update) # for annotation namespace as string
+
+        if value == "dc":
+            dc(content)
+        
+        if value == "route":
+            update_spec= content["spec"]["host"].split('.')
+            if "preprod" in DESTINATION_PROJECT:
+                update_spec[1] = "preprod"
+                host_name = '.'.join(update_spec)
+            elif "prod" in DESTINATION_PROJECT:
+                del update_spec[1]
+                host_name = '.'.join(update_spec)
+        
+            content["spec"]["host"] = host_name
+
         f = open(f'{value}_{each_config}.yaml', 'w+')
         yaml.dump(content, f, allow_unicode=True, default_flow_style=False)
 
-cmds = ["dc", "bc", "is", "route", "svc"]
+# cmds = ["dc", "bc", "is", "route", "svc"]
+cmds = ["dc"]
 for each_cmd in cmds:
     common(each_cmd)
+
 
 # def cleanup():
 #     os.system('rm file.yaml')
